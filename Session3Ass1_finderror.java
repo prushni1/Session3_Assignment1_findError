2c1c27e161c066755631bdbf57f65318068d825c
prushni1 <jprushni@gmail.com>
2016-08-29 12:13:45 +0530

    Add files via upload

---
+++ Session3Ass1_finderror.java
@@ -0,0 +1,35 @@
+
+public class Session3Ass1_finderror {
+	
+/*
+ * 
+
+What will happen if you try to compile and run this?
+
+public class Test{
+static{
+print(10);
+}
+static void print (int x) {
+System.out.println (x);
+System.exit(0); 
+}
+}
+
+*/
+	static{
+		print(10);
+		}
+		static void print (int x) {
+		System.out.println (x);
+		System.exit(0); 
+		}
+
+}
+
+
+/*
+ *  OUTPUT:
+ *   COMPILER ERROR
+ *
+ */
